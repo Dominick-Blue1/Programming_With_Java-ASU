5b95b280c2ad57f57b707e9a1060e17117c650c4
Justin Blue <justin.d.blue1@gmail.com>
2020-09-15 21:43:39 -0700

    commit

---
+++ Assignment04/Assignment04.java
@@ -0,0 +1,19 @@
+/*  CSE 110      
+*   Assignment   : Assignment04
+*   Author       : Justin Blue
+*   Description  : Number Guessing Game Using Nested Loops
+*/
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Assignment04
+{
+    public static void main(String[] args) 
+    {
+        Scanner scan = new Scanner(System.in);
+        Random random = new Random();
+        random.nextInt(10000);
+        
+    }
+}
